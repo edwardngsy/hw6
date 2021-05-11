7e67d33690e3f65a76f981a51386b7e13ed5cbe7
edward <edward.ng@kellogg.northwestern.edu>
2021-05-11 00:36:45 -0500

    updates

--- netlify/functions/movies.js
+++ netlify/functions/movies.js
@@ -3,6 +3,7 @@ let csv = require('neat-csv')
 
 // allows us to read files from disk
 let fs = require('fs')
+const { stringify } = require('querystring')
 
 // defines a lambda function
 exports.handler = async function(event) {
@@ -17,6 +18,9 @@ exports.handler = async function(event) {
 
   // write the movies to the back-end console, check it out
   console.log(moviesFromCsv)
+
+  // write the number of movies to the backend console
+  console.log(`There are ${moviesFromCsv.length} movies in the dataset.`)
 
   // 🔥 hw6: your recipe and code starts here!
   let year = event.queryStringParameters.year
@@ -33,15 +37,34 @@ exports.handler = async function(event) {
       numResults: 0,
       movies: []
     }
+  
+    // create a new object to hold the count and movies data
+  let moviesToReturn = {}
+
+  // start with an empty Array for the movies
+  moviesToReturn.movies = []
 
     for (let i=0; i < moviesFromCsv.length; i++) {
-
+      // store each movie in memory
+      let movie = moviesFromCsv[i]
+      // check genre and year 
+      if (movie.startYear == year && movie.genres == genre && movie.runtimeMinutes!== (`\\N`) ) {
+      // add the movie to the Array of movies to return
+      moviesToReturn.movies.push(movie)
     }
+    // add the number of movies to the count
+    moviesToReturn.numResults = moviesToReturn.movies.length
+  }
+  console.log(`numResults = ${moviesToReturn.numResults}.`)
+  console.log((`There are ${moviesToReturn.numResults} movies in the ${genre} genre in ${year}.`))
+
+
+    
 
     // a lambda function returns a status code and a string of data
     return {
       statusCode: 200, // https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
-      body: `Hello from the back-end!` // a string of data
+      body: JSON.stringify(moviesToReturn) // a string of data
     }
   }
 }
